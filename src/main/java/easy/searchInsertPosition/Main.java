1e1c81329d50bf556e9fee381d08fd6b832f340b
y.petrov <y.petrov@bftcom.com>
2022-10-18 11:24:21 +0300

    Решение SearchInsertPosition

--- src/main/java/easy/searchInsertPosition/Main.java
+++ src/main/java/easy/searchInsertPosition/Main.java
@@ -5,9 +5,9 @@ class Main {
   public static void main(String[] args) {
     Solution solution = new Solution();
 
-    int[] inputArray = {1,3};
-    int target = 4;
-    int expectedResult = 2;
+    int[] inputArray = {1,3,5,6,8,12,13,16};
+    int target = 8;
+    int expectedResult = 4;
     System.out.println(solution.searchInsert(inputArray, target) == expectedResult);
   }
 }
--- src/main/java/easy/searchInsertPosition/Solution.java
+++ src/main/java/easy/searchInsertPosition/Solution.java
@@ -1,46 +1,19 @@
 package main.java.easy.searchInsertPosition;
 
 class Solution {
-
   public int searchInsert(int[] nums, int target) {
-    int arrayLength = nums.length;
-    int indexToCheck = arrayLength / 2;
-    int indexGap = indexToCheck / 2;
-    int resultIndex = indexToCheck;
-    if (target == 0) {
-      return 0;
+    int leftIndex = 0;
+    int rightIndex = nums.length - 1;
+    int indexToCheck;
+    while (leftIndex <= rightIndex) {
+      indexToCheck = (leftIndex + rightIndex) / 2;
+      if(nums[indexToCheck] == target)
+        return indexToCheck;
+      else if(nums[indexToCheck] > target)
+        rightIndex = indexToCheck - 1;
+      else
+        leftIndex = indexToCheck + 1;
     }
-    if (resultIndex == 0) {
-      if (target <= nums[0]) return 0;
-      else return 1;
-    }
-    while (indexGap >= 1) {
-      resultIndex = indexToCheck;
-      if (resultIndex > arrayLength - 1) return resultIndex;
-      if (nums[resultIndex] > target) {
-        indexGap = indexToCheck / 2;
-        indexToCheck -= indexGap;
-      } else if (nums[resultIndex] < target) {
-        indexGap = indexToCheck / 2;
-        indexToCheck += indexGap;
-        if (indexToCheck < 1 && indexGap < 1) {
-          return 0;
-        }
-      } else if (nums[resultIndex] == target) {
-        return resultIndex ;
-      }
-    }
-    if (nums[resultIndex] > target && nums[resultIndex - 1] == target) {
-      return --resultIndex;
-    } else if (nums[resultIndex] > target) {
-      return resultIndex;
-    } else if (arrayLength > resultIndex + 1 && nums[resultIndex] < target && nums[resultIndex + 1] < target) {
-      return resultIndex + 2;
-    } else if (nums[resultIndex] < target) {
-      return ++resultIndex;
-    } else if (nums[resultIndex] == target) {
-      return resultIndex;
-    }
-    return resultIndex;
+    return leftIndex;
   }
 }
